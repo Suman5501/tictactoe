cd15ff791ae3ae62890f667dda9fc43c296ef51e
Suman Kumari <sumankri5501@gmail.com>
2020-10-11 19:25:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //0=circle 1=cross
+    int activePlayer = 0;
+    boolean gameActive = true;
+    int[] gameState={2,2,2,2,2,2,2,2,2};
+
+    int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+
+    //2 unplayed
+
+    public void dropIn(View view){
+
+        ImageView counter= (ImageView) view;
+
+        int tappedCounter=Integer.parseInt(counter.getTag().toString());
+        if (gameState[tappedCounter] == 2) {
+
+            gameState[tappedCounter]= activePlayer;
+
+        //counter.setTranslationY(-1000f);
+       // counter.animate().translationYBy(1000f).setDuration(300);
+
+        if(activePlayer==0){
+            counter.setImageResource(R.drawable.circle);
+            activePlayer=1;
+        }
+        else{
+            counter.setImageResource(R.drawable.cross);
+            activePlayer=0;
+        }
+
+        for(int[] winningPosition : winningPositions){
+            if(gameState[winningPosition[0]]==gameState[winningPosition[1]] &&
+                    gameState[winningPosition[1]]==gameState[winningPosition[2]] &&
+                    gameState[winningPosition[0]]!=2 ){
+
+                gameActive = false;
+                String winner="Black cross";
+
+                if(gameState[winningPosition[0]] ==0){
+                    winner = "Blue knot";
+                }
+                TextView result =findViewById(R.id.textView);
+                result.setText(winner+" has Won!!");
+
+
+                LinearLayout layout =(LinearLayout)findViewById(R.id.playAgainLayout);
+                layout.setVisibility(View.VISIBLE);
+                break;
+
+            }else{
+                boolean gameIsOver = true;
+
+                for(int counterState : gameState){
+                    if(counterState == 2){
+                        gameIsOver = false;
+                    }
+                }
+
+                if(gameIsOver){
+                    TextView result =findViewById(R.id.textView);
+                    result.setText("Its a draw!");
+
+
+                    LinearLayout layout =(LinearLayout)findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                }
+            }
+
+        }
+
+    }
+    }
+    public void playAgain(View view){
+        gameActive= true;
+
+        LinearLayout layout =(LinearLayout)findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+        activePlayer=0;
+
+        for(int i=0;i< gameState.length;i++){
+            gameState[i]=2;
+        }
+        GridLayout gridLayout =(GridLayout)findViewById(R.id.gridLayout);
+        for(int i=0; i<gridLayout.getChildCount();i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+
+
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
